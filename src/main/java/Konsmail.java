6fe805fab5da4523ea729b475316e04deaca05ce
narwa <narwandaru.nara@gmail.com>
2023-12-12 22:58:21 +0700

    refactor: remove remaining unnecessary instructions

--- src/main/java/Konsmail.java
+++ src/main/java/Konsmail.java
@@ -10,8 +10,5 @@ public class Konsmail {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        boolean e = Checker.areEqual("omga", "omega");
-        System.out.println(e);
     }
 }
