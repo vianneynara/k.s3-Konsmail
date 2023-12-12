3fefb0aa9eb9d496e81c35d05b7929da6faaa098
narwa <narwandaru.nara@gmail.com>
2023-12-12 18:28:58 +0700

    fix: gap on pack()
    Fixed (somehow) frame initializer gap that occured yesterday. Now back to normal.

--- src/main/java/gui/LoginPage.java
+++ src/main/java/gui/LoginPage.java
@@ -136,10 +136,10 @@ public class LoginPage extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(MAIN_PANEL, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(28, 28, 28))
+                .addGap(0, 0, 0))
         );
 
-        setSize(new java.awt.Dimension(416, 350));
+        pack();
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
