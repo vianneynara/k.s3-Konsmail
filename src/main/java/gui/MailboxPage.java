e2bbfbb3e3461162461166f9f7048e1bec39788a
narwa <narwandaru.nara@gmail.com>
2023-12-13 20:57:06 +0700

    refactor: update main methods for each class

--- src/main/java/gui/MailboxPage.java
+++ src/main/java/gui/MailboxPage.java
@@ -4,9 +4,11 @@
  */
 package gui;
 
-import java.awt.Image;
+import utils.USwingAppearance;
+
+import javax.swing.*;
+import java.awt.*;
 import java.util.Objects;
-import javax.swing.ImageIcon;
 
 /**
  *
@@ -333,27 +335,7 @@ public class MailboxPage extends javax.swing.JFrame {
      */
     public static void main(String[] args) {
         /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MailboxPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MailboxPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MailboxPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MailboxPage.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+        USwingAppearance.setLooksAndFeel();
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
