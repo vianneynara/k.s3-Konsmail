dbdae59c2bf78ad2596ae5475f0563d2861f497b
narwa <narwandaru.nara@gmail.com>
2023-12-14 18:54:43 +0700

    feat: added email and password checker

--- src/main/java/utils/DatabaseUtil.java
+++ src/main/java/utils/DatabaseUtil.java
@@ -12,7 +12,9 @@ public class DatabaseUtil {
         ArrayList<Email> mailbox = new ArrayList<Email>();
         String query = "SELECT * FROM MAILBOX";
 
-        try (Connection conn = DatabaseConnector.getConnection(); PreparedStatement ps = conn.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
+        try (Connection conn = DatabaseConnector.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query);
+                ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 String src = rs.getString("source");
@@ -34,7 +36,9 @@ public class DatabaseUtil {
         ArrayList<Account> accounts = new ArrayList<Account>();
         String query = "SELECT * FROM ACCOUNTS";
 
-        try (Connection conn = DatabaseConnector.getConnection(); PreparedStatement ps = conn.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
+        try (Connection conn = DatabaseConnector.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query);
+                ResultSet rs = ps.executeQuery()) {
 
             while (rs.next()) {
                 String fName = rs.getString("first_name");
@@ -53,10 +57,11 @@ public class DatabaseUtil {
     }
 
     public static void insertEmail(Email mail) {
-        String query = "INSERT INTO MAILBOX"
-                + "VALUES (?, ?, ?, ?, ?)";
+        String query = "INSERT INTO MAILBOX " +
+                "VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DatabaseConnector.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+        try (Connection conn = DatabaseConnector.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)) {
 
             ps.setString(1, mail.getSender());
             ps.setString(2, mail.getRecipient());
@@ -71,10 +76,11 @@ public class DatabaseUtil {
     }
 
     public static void insertAccount(Account acct) {
-        String query = "INSERT INTO ACCOUNTS "
-                + "VALUES (?, ?, ?, ?, ?)";
+        String query = "INSERT INTO ACCOUNTS " +
+                "VALUES (?, ?, ?, ?, ?)";
 
-        try (Connection conn = DatabaseConnector.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
+        try (Connection conn = DatabaseConnector.getConnection();
+                PreparedStatement ps = conn.prepareStatement(query)) {
 
             ps.setString(1, acct.getUuid());
             ps.setString(2, acct.getEmailAddress());
@@ -84,49 +90,49 @@ public class DatabaseUtil {
 
             ps.executeUpdate();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
     public static boolean accountExists(String address) {
         String query = "SELECT email_address FROM ACCOUNTS WHERE email_address = ?";
 
-        try (Connection conn = DatabaseConnector.getConnection(); PreparedStatement ps = conn.prepareStatement(query)) {
-
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            // sets the first question mark to the email address
             ps.setString(1, address);
 
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    System.out.println("Account exists");
-                    return true;
-                }
-            }
+            // executes the query and returns true if the result set is not empty
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            ULogger.logError(e, "Failed to check if account exists.");
+            return false;
         }
-        return false;
     }
 
+    /**
+     * Checks if the email address and password matches in the database.
+     *
+     * @param address The email address to be checked.
+     * @param password The password to be checked.
+     * */
     public static boolean emailAndPasswordMatches(String address, String password) {
-        String query = "SELECT email_address FROM ACCOUNTS "
-                + "WHERE email_address = ? AND password = ?";
+        String query = "SELECT email_address FROM ACCOUNTS WHERE email_address = ? AND password = ?";
 
-        try (Connection conn = DatabaseConnector.getConnection(); 
-                PreparedStatement ps = conn.prepareStatement(query)) {
-
+        try (Connection conn = DatabaseConnector.getConnection();
+             PreparedStatement ps = conn.prepareStatement(query)) {
+            // sets the first question mark to the email address
             ps.setString(1, address);
             ps.setString(2, password);
 
-            try (ResultSet rs = ps.executeQuery()) {
-                if (rs.next()) {
-                    System.out.println("email and password matches");
-                    return true;
-                }
-            }
+            // executes the query and returns true if the result set is not empty
+            ResultSet rs = ps.executeQuery();
+            return rs.next();
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            ULogger.logError(e, "Failed to check if account exists.");
+            return false;
         }
-        return false;
     }
 
 }
