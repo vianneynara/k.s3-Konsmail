6909284f6ab246efe213c757078eedf856f753e5
narwa <narwandaru.nara@gmail.com>
2023-12-15 13:06:54 +0700

    feat(ComponentUtils): added foreground color setters

--- src/main/java/utils/ComponentUtils.java
+++ src/main/java/utils/ComponentUtils.java
@@ -11,24 +11,24 @@ import java.awt.Component;
 
 public class ComponentUtils {
 	/**
-	 * Modifies the background color of the passed {@link Component}s using the passed {@link Color}.
+	 * Modifies the background color of the passed {@link Component}s using the passed {@link Color} simultaneously.
 	 *
 	 * @param color The color to be set.
 	 * @param components The components to be modified.
 	 * */
-	public static void setComponentsColor(Color color, Component... components) {
+	public static void setBackgroundColor(Color color, Component... components) {
 		for (Component component : components) {
 			component.setBackground(color);
 		}
 	}
 
 	/**
-	 * Modifies the background color of the passed {@link Component}s using the passed hex code.
+	 * Modifies the background color of the passed {@link Component}s using the passed hex code simultaneously.
 	 *
 	 * @param colorHex The hex color to be set.
 	 * @param components The components to be modified.
 	 * */
-	public static void setComponentsColor(int colorHex, Component... components) {
+	public static void setBackgroundColor(int colorHex, Component... components) {
 		// checks whether the hex format is valid
 		if (colorHex < 0 || colorHex > 0xFFFFFF) {
 			throw new IllegalArgumentException("Invalid hex format.");
@@ -37,6 +37,36 @@ public class ComponentUtils {
 
 		for (Component component : components) {
 			component.setBackground(color);
+		}
+	}
+
+	/**
+	 * Modifies the foreground color of the passed {@link Component}s using the passed {@link Color} simultaneously.
+	 *
+	 * @param color The color to be set.
+	 * @param components The components to be modified.
+	 * */
+	public static void setForegroundColor(Color color, Component... components) {
+		for (Component component : components) {
+			component.setForeground(color);
+		}
+	}
+
+	/**
+	 * Modifies the foreground color of the passed {@link Component}s using the passed hex code simultaneously.
+	 *
+	 * @param colorHex The hex color to be set.
+	 * @param components The components to be modified.
+	 * */
+	public static void setForegroundColor(int colorHex, Component... components) {
+		// checks whether the hex format is valid
+		if (colorHex < 0 || colorHex > 0xFFFFFF) {
+			throw new IllegalArgumentException("Invalid hex format.");
+		}
+		Color color = new Color(colorHex);
+
+		for (Component component : components) {
+			component.setForeground(color);
 		}
 	}
 }
