b3ec273e4ba57336e2bf267fd0047516512c6c3c
narwa <narwandaru.nara@gmail.com>
2023-12-17 00:13:31 +0700

    refactor: remove database sender and receiver reference

--- src/main/database/sqlfiles/CreateAccountsTable.sql
+++ src/main/database/sqlfiles/CreateAccountsTable.sql
@@ -2,11 +2,10 @@
 CREATE TABLE Accounts
 (
     uuid          VARCHAR2(36)
-        CONSTRAINT accounts_PK PRIMARY KEY          NOT NULL,
-    email_address VARCHAR2(255)
-        CONSTRAINT accounts_email_address_UK UNIQUE NOT NULL,
+        CONSTRAINT accounts_PK PRIMARY KEY NOT NULL,
+    email_address VARCHAR2(255),
     password      VARCHAR2(128)
-                                                    NOT NULL,
+                                           NOT NULL,
     first_name    VARCHAR2(50)
         CONSTRAINT accounts_first_name_NN NOT NULL,
     last_name     VARCHAR2(50)
--- src/main/database/sqlfiles/CreateMailboxTable.sql
+++ src/main/database/sqlfiles/CreateMailboxTable.sql
@@ -1,16 +1,16 @@
 -- script to create tabele Mailbox
 CREATE TABLE Mailbox
 (
-    uuid VARCHAR2(36)
+    uuid           VARCHAR2(36)
         CONSTRAINT mailbox_PK PRIMARY KEY NOT NULL,
-    sender_uuid VARCHAR2(36)
-        REFERENCES Accounts (uuid) NOT NULL,
-    recipient_uuid VARCHAR2(36)
-        REFERENCES Accounts (uuid) NOT NULL,
-    subject VARCHAR2(128) ,
-    content VARCHAR2(1024),
-    timestamp TIMESTAMP
+    sender_uuid    VARCHAR2(36),
+    recipient_uuid VARCHAR2(36),
+    subject        VARCHAR2(128),
+    content        VARCHAR2(1024),
+    timestamp      TIMESTAMP
         CONSTRAINT mailbox_timestamp_NN NOT NULL,
-    is_read NUMBER(1, 0) NOT NULL,                                             
-    is_ad NUMBER(1, 0) NOT NULL
+    is_read        NUMBER(1, 0)
+        NOT NULL,
+    is_ad          NUMBER(1, 0)
+        NOT NULL
 );
